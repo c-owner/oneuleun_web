c87d7a62f7ff1bf02b4c72b8bc382b05b9e3d606
unknown <tjwns3481@gmail.com>
2022-06-25 14:35:12 +0900

    라우터 조금만 작업한거임

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Main from "./views/Main";
+import Hello from "./views/Hello"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          안녕하세요 빌런준이예요. 수정2
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <div id="body" className="diary-app-body">
+              <Routes>
+                  <Route path="/" element={<Main />} />
+                  <Route path="/hello" element={<Hello/>} />
+              </Routes>
+          </div>
+      </div>
   );
 }
 
---
+++ src/views/Hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hello = () => {
+  return (
+    <div>
+      안녕하세요
+    </div>
+  )
+}
+
+export default Hello;
