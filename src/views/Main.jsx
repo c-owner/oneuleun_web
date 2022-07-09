4864a44b92a7b2ee94258308bf38cdde72c7097c
corner <corner3499@gmail.com>
2022-07-09 17:39:30 +0900

    절대경로 setup

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+
 const Main = () => {
   return (
     <div>
-      안녕하세요
-      <Link to='/hello'>헬로우</Link>
+		
+
     </div>
   )
 }
