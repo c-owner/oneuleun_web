1b76a937f2090fdc1a3d7ed6163ae11479601484
corner <corner3499@gmail.com>
2022-07-09 18:59:11 +0900

    main css

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,6 +1,7 @@
 const Main = () => {
   return (
     <div class="intro">
+		<div className="dark-bg w100p h100p p-absoulte "></div>
 		<div className="p-relative w100p pt60">
 			<p className="title line-1 anim-typewriter">오늘 하루 어떠셨나요?. . . .</p>
 		</div>
