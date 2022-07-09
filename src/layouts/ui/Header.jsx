26353623e8f32c677dad6243142bfb8d40a770fd
corner <corner3499@gmail.com>
2022-07-09 18:01:09 +0900

    headers

--- src/layouts/ui/Header.jsx
+++ src/layouts/ui/Header.jsx
@@ -1,11 +1,68 @@
-import React from 'react';
+import logo_dark from 'assets/images/logo_dark.png'
+import React from "react";
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const Header = () => {
-  return (
-    <div className="flex space-between">
+	const [anchorElNav, setAnchorElNav] = React.useState(null);
+	const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-    </div>
-  )
+	const handleOpenNavMenu = (event) => {
+		setAnchorElNav(event.currentTarget);
+	};
+	const handleOpenUserMenu = (event) => {
+		setAnchorElUser(event.currentTarget);
+	};
+
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
+
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
+	};
+
+	return (
+		<div className="header flex space-between item-center">
+			<img src={logo_dark} alt="오늘은 로고" width="82" height="50" />
+			<Box sx={{ flexGrow: 0 }}>
+				<Tooltip title="Open settings">
+					<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+						<Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+					</IconButton>
+				</Tooltip>
+				<Menu
+					sx={{ mt: '45px' }}
+					id="menu-appbar"
+					anchorEl={anchorElUser}
+					anchorOrigin={{
+						vertical: 'top',
+						horizontal: 'right',
+					}}
+					keepMounted
+					transformOrigin={{
+						vertical: 'top',
+						horizontal: 'right',
+					}}
+					open={Boolean(anchorElUser)}
+					onClose={handleCloseUserMenu}
+				>
+					{settings.map((setting) => (
+						<MenuItem key={setting} onClick={handleCloseUserMenu}>
+							<Typography textAlign="center">{setting}</Typography>
+						</MenuItem>
+					))}
+				</Menu>
+			</Box>
+		</div>
+	)
 }
 
 export default Header
