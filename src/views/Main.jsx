1b714348b4c2a0058c66beb6b9159efbb5b9b48e
corner <corner3499@gmail.com>
2022-07-09 18:52:00 +0900

    setup

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,9 +1,9 @@
-import { Link } from 'react-router-dom';
-
 const Main = () => {
   return (
-    <div>
-		
+    <div class="intro">
+		<div className="p-relative w100p pt60">
+			<p className="title line-1 anim-typewriter">오늘 하루 어떠셨나요?. . . .</p>
+		</div>
 
     </div>
   )
