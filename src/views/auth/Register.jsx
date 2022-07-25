530185e7ba3a992d9cbfce34a24b27450aeef5ae
corner <corner3499@gmail.com>
2022-07-25 22:36:24 +0900

    register page

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import {TextField} from "@mui/material";
 
 const Register = () => {
-
+	const styleOptions = {
+		width: '100%',
+	}
 	return (
 		<div className="bg-white auth_wrap">
-
 			<div className="register_box">
+
+				<div className="register_form">
+					<div>
+						<TextField style={styleOptions} id="outlined-basic" label="이름" variant="outlined" />
+					</div>
+					<div>
+
+						<TextField id="outlined-basic" type={"email"} label="아이디" variant="outlined" />
+					</div>
+					<div>
+
+						<TextField id="outlined-basic" type={"password"} label="비밀번호" variant="outlined" />
+					</div>
+					<div>
+
+						<TextField id="outlined-basic" type={"password"} label="비밀번호 확인" variant="outlined" />
+					</div>
+				</div>
 
 			</div>
 		</div>
