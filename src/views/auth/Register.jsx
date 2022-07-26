63db8e123139c3c598eefa05207efc7c04bffd2d
corner <corner3499@gmail.com>
2022-07-26 09:52:28 +0900

    auth register form input class style

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -12,19 +12,25 @@ const Register = () => {
 					<form onSubmit={submit()} className="auth_form">
 						<div className="form_content">
 							<label for="user_email">이메일</label>
-							<input type="email" id="user_email" placeholder="이메일을 입력해주세요" />
+							<input className="input" type="email" id="user_email" placeholder="이메일을 입력해주세요" />
+							<button type="button" className="btn-1">인증 발송</button>
+						</div>
+						<div className="form_content">
+							<label for="user_email">인증</label>
+							<input className="input" type="email" id="user_email" placeholder="인증번호를 입력해주세요" />
+							<button type="button" className="btn-1">인증 확인</button>
 						</div>
 						<div className="form_content">
 							<label for="user_nickname">닉네임</label>
-							<input type="text" id="user_nickname" placeholder="닉네임을 입력해주세요.(2~12자)" />
+							<input className="input" type="text" id="user_nickname" placeholder="닉네임을 입력해주세요.(2~12자)" />
 						</div>
 						<div className="form_content">
 							<label for="user_passowrd">비밀번호</label>
-							<input type="password" id="user_passowrd" placeholder="비밀번호을 입력해주세요" />
+							<input className="input" type="password" id="user_passowrd" placeholder="비밀번호을 입력해주세요" />
 						</div>
 						<div className="form_content">
 							<label for="password_confirm">비밀번호 확인</label>
-							<input type="password" id="password_confirm" placeholder="비밀번호 확인" />
+							<input className="input" type="password" id="password_confirm" placeholder="비밀번호 확인" />
 						</div>
 
 					</form>
