a6233653748f880b0780b1be6f297470fc3288e2
Dasom Ko <ds92ko@gmail.com>
2022-08-02 19:06:35 +0900

    레이아웃 스타일 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Hello from "views/Hello";
 import Header from "layouts/ui/Header"
 import Register from "views/auth/Register";
 import Login from 'views/auth/Login';
+import Avatar from '@mui/material/Avatar';
+import ImportContactsIcon from '@mui/icons-material/ImportContacts';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
 
 const auth_path = '/auth';
 const diary_path = '/diary';
@@ -15,31 +18,53 @@ function App() {
 			<div id="header">
 				{<Header/>}
 			</div>
-			<div id="lnb">
-			</div>
 			<div id="body" className="oneuleun-app-body">
-				<Routes>
-					{/* 메인 */}
-					<Route path="/" element={<Main/>}/>
-					{/* 로그인 */}
-					<Route path={auth_path + '/login'} element={<Login/>}/>
-					{/* 회원가입 */}
-					<Route path={auth_path + '/register'} element={<Register/>}/>
-					{/* 일기장 */}
-					<Route path="/diary" element={<Hello/>}/>
-					{/* 일기장 작성 */}
-					<Route path="/diary/add" element={<Hello/>}/>
-					{/* 일기장 상세 */}
-					<Route path="/diary/:id" element={<Hello/>}/>
-					{/* 일기장 상세 수정 및 삭제 */}
-					<Route path="/diary/edit/:id" element={<Hello/>}/>
-					{/* 나의 감정 */}
-					<Route path="/emotion" element={<Hello/>}/>
-					{/* 마이페이지 */}
-					<Route path="/mypage" element={<Hello/>}/>
-					{/* 회원가입 */}
-					<Route path="/register" element={<Hello/>}/>
-				</Routes>
+				<div id="nav">
+					<div id="lnb">
+						<ul className='lnb-list'>
+							<li className='lnb-item'>
+								<a href="/diary" className='btn-lnb'>
+									<Avatar sx={{ background: '#fff', margin: '0 auto' }}>
+										<ImportContactsIcon  sx={{ color: '#333', fontSize: '2rem' }} />
+									</Avatar>
+									<span className='lnb-title'>일기장</span>
+								</a>
+							</li>
+							<li className='lnb-item'>
+								<a href="/emotion" className='btn-lnb'>
+									<Avatar sx={{ background: '#fff', margin: '0 auto' }}>
+										<SentimentSatisfiedAltIcon  sx={{ color: '#333', fontSize: '2rem' }} />
+									</Avatar>
+									<span className='lnb-title'>나의 기분</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div id="main">
+					<Routes>
+						{/* 메인 */}
+						<Route path="/" element={<Main/>}/>
+						{/* 로그인 */}
+						<Route path={auth_path + '/login'} element={<Login/>}/>
+						{/* 회원가입 */}
+						<Route path={auth_path + '/register'} element={<Register/>}/>
+						{/* 일기장 */}
+						<Route path="/diary" element={<Hello/>}/>
+						{/* 일기장 작성 */}
+						<Route path="/diary/add" element={<Hello/>}/>
+						{/* 일기장 상세 */}
+						<Route path="/diary/:id" element={<Hello/>}/>
+						{/* 일기장 상세 수정 및 삭제 */}
+						<Route path="/diary/edit/:id" element={<Hello/>}/>
+						{/* 나의 감정 */}
+						<Route path="/emotion" element={<Hello/>}/>
+						{/* 마이페이지 */}
+						<Route path="/mypage" element={<Hello/>}/>
+						{/* 회원가입 */}
+						<Route path="/register" element={<Hello/>}/>
+					</Routes>
+				</div>
 			</div>
 		</div>
 	);
--- src/layouts/ui/Header.jsx
+++ src/layouts/ui/Header.jsx
@@ -7,6 +7,7 @@ import Menu from '@mui/material/Menu';
 import Avatar from '@mui/material/Avatar';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
+import PaletteIcon from '@mui/icons-material/Palette';
 
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
@@ -17,46 +18,55 @@ const Header = () => {
 		setAnchorElUser(event.currentTarget);
 	};
 
-
 	const handleCloseUserMenu = () => {
 		setAnchorElUser(null);
 	};
 
 	return (
 		<div className="header flex space-between item-center">
-			<img src={logo_dark} alt="오늘은 로고" width="82" height="50"
-				 className="cursor" onClick={() => {
+			<img src={logo_dark} alt="오늘은 로고" width="82" height="50" className="cursor" onClick={() => {
 					window.location.href = '/';
 			}} />
-			<Box sx={{ flexGrow: 0 }}>
-				<Tooltip title="Open settings">
-					<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-						<Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-					</IconButton>
-				</Tooltip>
-				<Menu
-					sx={{ mt: '45px' }}
-					id="menu-appbar"
-					anchorEl={anchorElUser}
-					anchorOrigin={{
-						vertical: 'top',
-						horizontal: 'right',
-					}}
-					keepMounted
-					transformOrigin={{
-						vertical: 'top',
-						horizontal: 'right',
-					}}
-					open={Boolean(anchorElUser)}
-					onClose={handleCloseUserMenu}
-				>
-					{settings.map((setting) => (
-						<MenuItem key={setting} onClick={handleCloseUserMenu}>
-							<Typography textAlign="center">{setting}</Typography>
-						</MenuItem>
-					))}
-				</Menu>
-			</Box>
+			<div className='aside flex item-center'>
+				<Box sx={{ flexGrow: 0, marginRight: '15px' }}>
+					<Tooltip title="Open theme modal">
+						<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+							<Avatar sx={{ background: 'linear-gradient(#9074FF, #FFB9D1)' }}>
+								<PaletteIcon />
+							</Avatar>
+						</IconButton>
+					</Tooltip>
+				</Box>
+				<Box sx={{ flexGrow: 0 }}>
+					<Tooltip title="Open settings">
+						<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+							<Avatar sx={{ background: 'linear-gradient(#9074FF, #FFB9D1)' }} alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+						</IconButton>
+					</Tooltip>
+					<Menu
+						sx={{ mt: '45px' }}
+						id="menu-appbar"
+						anchorEl={anchorElUser}
+						anchorOrigin={{
+							vertical: 'top',
+							horizontal: 'right',
+						}}
+						keepMounted
+						transformOrigin={{
+							vertical: 'top',
+							horizontal: 'right',
+						}}
+						open={Boolean(anchorElUser)}
+						onClose={handleCloseUserMenu}
+					>
+						{settings.map((setting) => (
+							<MenuItem key={setting} onClick={handleCloseUserMenu}>
+								<Typography textAlign="center">{setting}</Typography>
+							</MenuItem>
+						))}
+					</Menu>
+				</Box>
+			</div>
 		</div>
 	)
 }
